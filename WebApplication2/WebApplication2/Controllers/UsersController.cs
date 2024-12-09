2a6ae801a37c9a868949713e16227d2ec1e72d06
Slice67 <slice67.dominik@gmail.com>
2024-12-09 04:50:17 +0100

    Logging changes
    Added logging changes of all attributes

--- WebApplication2/WebApplication2/Controllers/UsersController.cs
+++ WebApplication2/WebApplication2/Controllers/UsersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Threading.Channels;
 using WebApplication2.Models;
 
 namespace WebApplication2.Controllers {
@@ -34,16 +35,52 @@ namespace WebApplication2.Controllers {
 
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateUser(int id, [FromBody] User updatedUser) {
-            if (id != updatedUser.Id ) {
+            if ( id != updatedUser.Id ) {
                 return BadRequest();
             }
 
-            _context.Entry(updatedUser).State = EntityState.Modified;
+            var existingUser = await _context.Users.FindAsync(id);
+            if ( existingUser == null ) {
+                return NotFound();
+            }
+
+            var changes = new List<ChangeLog>();
+
+            void LogChange(string attribute, string oldValue, string newValue) {
+                if ( oldValue != newValue ) {
+                    changes.Add(new ChangeLog {
+                        UserId = id,
+                        Operation = "Updated",
+                        ChangedAttribute = attribute,
+                        OldValue = oldValue,
+                        NewValue = newValue,
+                        ChangeDate = DateTime.UtcNow
+                    });
+                }
+            }
+
+            LogChange("Name", existingUser.Name, updatedUser.Name);
+            LogChange("Surname", existingUser.Surname, updatedUser.Surname);
+            LogChange("Email", existingUser.Email, updatedUser.Email);
+            LogChange("Telephone", existingUser.Telephone, updatedUser.Telephone);
+            LogChange("Active", existingUser.Active.ToString(), updatedUser.Active.ToString());
+
+            _context.Entry(existingUser).CurrentValues.SetValues(updatedUser);
+            _context.Entry(existingUser).State = EntityState.Modified;
+            _context.ChangeLogs.AddRange(changes);
+
+            Console.WriteLine("Logované změny: ");
+            foreach ( var change in changes ) {
+                Console.WriteLine(
+                    $"Attribute:{change.ChangedAttribute}, " +
+                    $"OldValue: {change.OldValue}, " +
+                    $"NewValue: {change.NewValue}");
+            }
 
             try {
                 await _context.SaveChangesAsync();
             } catch ( DbUpdateConcurrencyException ) {
-                if (!_context.Users.Any(e => e.Id == id) ) {
+                if ( !_context.Users.Any(e => e.Id == id) ) {
                     return NotFound();
                 } else {
                     throw;
@@ -51,5 +88,23 @@ namespace WebApplication2.Controllers {
             }
             return NoContent();
         }
+        /*
+        [HttpPost("test-log")]
+        public async Task<IActionResult> TestChangeLog() {
+            var testChange = new ChangeLog {
+                UserId = 0, // Můžete použít jakékoliv ID, které má smysl pro testování
+                Operation = "Test",
+                ChangedAttribute = "TestAttribute",
+                OldValue = "OldTestValue",
+                NewValue = "NewTestValue",
+                ChangeDate = DateTime.UtcNow
+            };
+
+            _context.ChangeLogs.Add(testChange);
+            await _context.SaveChangesAsync();
+
+            return Ok("ChangeLog záznam byl úspěšně přidán.");
+        }
+        */
     }
 }
--- WebApplication2/WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
+++ WebApplication2/WebApplication2/obj/Debug/net8.0/WebApplication2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf851769e1fe2e1bd51fe829d8d48682f778fb2f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2d9b5e142d3f7104bdf04e7972b9002f688daf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
